b73701b0ed7def83a0fd0e76a7425ac42ce085f3
Biggs Thorarensen <biggs@devillion.com>
2018-05-02 09:14:42 +0100

    routes hookup (routes and nav)

--- routes/index.js
+++ routes/index.js
@@ -35,11 +35,13 @@ var routes = {
 exports = module.exports = function (app) {
 	// Views
 	app.get('/', routes.views.index);
-	app.get('/blog/:category?', routes.views.blog);
-	app.get('/blog/post/:post', routes.views.post);
-	app.get('/gallery', routes.views.gallery);
-    app.all('/contact', routes.views.contact);
-	app.all('/test', routes.views.test);
+	//app.get('/blog/:category?', routes.views.blog);
+	//app.get('/blog/post/:post', routes.views.post);
+	//app.get('/gallery', routes.views.gallery);
+    //app.all('/contact', routes.views.contact);
+	app.all('/audioplate', routes.views.audioplate);
+	app.all('/consumed', routes.views.consumed);
+	app.all('/artist/:artist?', routes.views.artist);
 
 	// NOTE: To protect a route so that only admins can see it, use the requireUser middleware:
 	// app.get('/protected', middleware.requireUser, routes.views.protected);
--- routes/middleware.js
+++ routes/middleware.js
@@ -20,10 +20,12 @@ var _ = require('lodash');
 exports.initLocals = function (req, res, next) {
 	res.locals.navLinks = [
 		{ label: 'Home', key: 'home', href: '/' },
-		{ label: 'Blog', key: 'blog', href: '/blog' },
-		{ label: 'Gallery', key: 'gallery', href: '/gallery' },
-		{ label: 'Contact', key: 'contact', href: '/contact' },
-		{ label: 'Test', key: 'test', href: '/test' },
+		//{ label: 'Blog', key: 'blog', href: '/blog' },
+		//{ label: 'Gallery', key: 'gallery', href: '/gallery' },
+		//{ label: 'Contact', key: 'contact', href: '/contact' },
+		{ label: 'Audioplate', key: 'audioplate', href: '/audioplate' },
+		{ label: 'Consumed', key: 'consumed', href: '/consumed' },
+		{ label: 'Artists', key: 'artist', href: '/artist' },
 	];
 	res.locals.user = req.user;
 	next();

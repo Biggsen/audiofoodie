2dee8f5d729ef0958db6c2376f7876c3779fffc2
Biggs Thorarensen <biggs@devillion.com>
2019-09-10 13:07:20 +0100

    old status dates in ArtistAlbum model removed

--- models/ArtistAlbum.js
+++ models/ArtistAlbum.js
@@ -21,28 +21,19 @@ ArtistAlbum.add({
     newArtist: { type: Types.Boolean, label: 'Is this a new artist?' },
     user: { type: Types.Relationship, ref: 'User', many: true},
     filter1Date: { type: Types.Date, default: '' },
-    curiousDate: { type: Types.Date, default: '' },
     storage1Date: { type: Types.Date, default: '' },
-    notInterestedDate: { type: Types.Date, default: '' },
     filter2Date: { type: Types.Date, default: '' },
-    interestedDate: { type: Types.Date, default: '' },
     storage2Date: { type: Types.Date, default: '' },
-    decentStuffDate: { type: Types.Date, default: '' },
     filter3Date: { type: Types.Date, default: '' },
-    greatDate: { type: Types.Date, default: '' },
     storage3Date: { type: Types.Date, default: '' },
-    greatNotGrippingDate: { type: Types.Date, default: '' },
     filter4Date: { type: Types.Date, default: '' },
-    excellentDate: { type: Types.Date, default: '' },
     storage4Date: { type: Types.Date, default: '' },
-    notQuiteWonderfulDate: { type: Types.Date, default: '' },
     filter5Date: { type: Types.Date, default: '' },
-    wonderfulDate: { type: Types.Date, default: '' },
     movement: { type: Types.Boolean, label: 'Flag movement' },
     movementDate: { type: Types.Date, default: '' }
 });
 
 ArtistAlbum.relationship({ ref: 'Artist', path: 'artists', refPath: 'album' });
-ArtistAlbum.defaultColumns = 'name, year, status, -createdAt, movementDate, excellentDate, filter4Date';
-ArtistAlbum.defaultSort = '-excellentDate';
+ArtistAlbum.defaultColumns = 'name, year, status, -createdAt, movementDate';
+ArtistAlbum.defaultSort = '-createdAt';
 ArtistAlbum.register();
